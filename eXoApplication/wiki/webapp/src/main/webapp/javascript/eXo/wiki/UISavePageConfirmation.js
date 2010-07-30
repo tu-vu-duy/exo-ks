216ca91c43439632517364510b8c6012fbf10617
hieu_lai <noreply@exoplatform.com>
2010-07-30 10:18:22 +0000

    KS-2620: Drag&Drop Ability for Confirm Box

--- eXoApplication/wiki/webapp/src/main/webapp/javascript/eXo/wiki/UISavePageConfirmation.js
+++ eXoApplication/wiki/webapp/src/main/webapp/javascript/eXo/wiki/UISavePageConfirmation.js
@@ -17,32 +17,43 @@
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
 
-function UISavePageValidator() {
+function UISavePageConfirmation() {	
 };
 
 /*ie bug  you cannot have more than one button tag*/
 /**
  * Submits a form with the given action and the given parameters
  */
-UISavePageValidator.prototype.validateSave = function(pageTitleinputId,currentMode) {
+
+UISavePageConfirmation.prototype.validateSave = function(pageTitleinputId,currentMode) {
 	var ConfirmMask= document.getElementById("ConfirmMask");
 	var pageTitleInput= document.getElementById(pageTitleinputId);
 	if ((currentMode=="NEW")&&(pageTitleInput.value=="Untitle")){
 		ConfirmMask.style.display="block";
-		document.getElementById("ConfirmMessage").innerHTML="You are about to save an Untitled page.";
+		var ConfirmMessage= eXo.core.DOMUtil.findFirstDescendantByClass(ConfirmMask,"div", "ConfirmMessage");
+		ConfirmMessage.innerHTML="You are about to save an Untitled page.";
 		return false;
 	}
 	else if (currentMode=="EDIT"){
 		ConfirmMask.style.display="block";	
-		document.getElementById("ConfirmMessage").innerHTML="Your changes will be saved to history.<br/>Are you sure you want to apply this changes?"
+		var ConfirmMessage= eXo.core.DOMUtil.findFirstDescendantByClass(ConfirmMask,"div", "ConfirmMessage");
+		ConfirmMessage.innerHTML="Your changes will be saved to history.<br/>Are you sure you want to apply this changes?";		
 		return false;
 	}
 	return true;
 } ;
 
-UISavePageValidator.prototype.closeConfirm =function(){
+UISavePageConfirmation.prototype.closeConfirm =function(){
 		var ConfirmMask= document.getElementById("ConfirmMask");
 		ConfirmMask.style.display="none";
+		return false;
 	}
-
-eXo.wiki.UISavePageValidator = new UISavePageValidator();
+UISavePageConfirmation.prototype.initDragDrop = function(){		
+	var ConfirmBox= document.getElementById("ConfirmBox");
+	var ConfirmBox = eXo.core.DOMUtil.findFirstDescendantByClass(ConfirmMask,"div", "ConfirmBox");
+	var ConfirmTitle = eXo.core.DOMUtil.findFirstDescendantByClass(ConfirmMask,"div", "ConfirmTitle");
+	ConfirmTitle.onmousedown = function(e){
+		eXo.core.DragDrop.init(null,ConfirmTitle,ConfirmBox,e);			
+	}	
+}
+eXo.wiki.UISavePageConfirmation = new UISavePageConfirmation();
